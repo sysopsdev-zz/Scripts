aa9ebf6508a78694dd8d833a2f1960c1d45dc0a0
Chris Kennedy <35941012+sysopsdev@users.noreply.github.com>
2018-03-21 08:28:33 -0500

    Upgrade OS and Kernel on CentOS UEFI based system

---
+++ uefi-upgrade.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+yum update -y
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org 
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+yum --enablerepo=elrepo-kernel install kernel-ml -y
+sed -i 's/saved/0/g' /etc/default/grub
+grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg
+reboot
